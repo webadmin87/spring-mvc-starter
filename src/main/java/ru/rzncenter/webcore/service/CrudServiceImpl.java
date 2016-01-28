109f09486395b1521590bcd638a33f52b11105dc
Anton office <webadmin87@gmail.com>
2016-01-28 15:07:35 +0300

    присвоение автора пользователю при сохранении

--- src/main/java/ru/rzncenter/webcore/service/CrudServiceImpl.java
+++ src/main/java/ru/rzncenter/webcore/service/CrudServiceImpl.java
@@ -1,10 +1,15 @@
 package ru.rzncenter.webcore.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.transaction.annotation.Transactional;
+import ru.rzncenter.webcore.dao.UserDao;
+import ru.rzncenter.webcore.domains.Domain;
 
 import java.lang.reflect.ParameterizedType;
 import java.util.List;
@@ -13,7 +18,10 @@ import java.util.List;
  * Сервис для CRUD операций
  * @param <T>
  */
-public abstract class CrudServiceImpl<T> implements CrudService<T> {
+public abstract class CrudServiceImpl<T extends Domain> implements CrudService<T> {
+
+    @Autowired
+    UserDao userDao;
 
     protected Class<T> getGenericSuperClass() {
 
@@ -46,6 +54,7 @@ public abstract class CrudServiceImpl<T> implements CrudService<T> {
     @Override
     @Transactional
     public T save(T domain) {
+        applyAuthor(domain);
         return getRepository().save(domain);
     }
 
@@ -57,4 +66,25 @@ public abstract class CrudServiceImpl<T> implements CrudService<T> {
 
     @Override
     public abstract JpaRepository<T, Long> getRepository();
+
+    public UserDao getUserDao() {
+        return userDao;
+    }
+
+    public void setUserDao(UserDao userDao) {
+        this.userDao = userDao;
+    }
+
+    protected void applyAuthor(T domain) {
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        if(domain.getAuthor() == null && authentication.isAuthenticated()) {
+
+            domain.setAuthor(userDao.findByUsername(authentication.getName()));
+
+        }
+
+    }
+
 }
