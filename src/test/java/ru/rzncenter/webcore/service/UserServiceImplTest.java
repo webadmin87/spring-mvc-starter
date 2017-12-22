c3ed7aef5cd3c5ebd0ab42084950c6b91b56d634
Anton nout <webadmin87@gmail.com>
2017-12-22 19:14:15 +0300

    фикс тестов

--- src/test/java/ru/rzncenter/webcore/service/UserServiceImplTest.java
+++ src/test/java/ru/rzncenter/webcore/service/UserServiceImplTest.java
@@ -5,10 +5,12 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.util.Assert;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration("classpath:spring-test.xml")
+@WebAppConfiguration
 public class UserServiceImplTest {
 
     private static final String GOOD_USERNAME = "admin";
